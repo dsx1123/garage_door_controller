5efed4392e29541864e08e67f6113a85ad921f7a
root <shangxindu@gmail.com>
2016-12-29 17:34:36 -0800

    first working version

--- controller.py
+++ controller.py
@@ -5,16 +5,18 @@ __email__="shangxindu@gmail.com"
 
 import logging
 import time
+from datetime import datetime
 import json
 from logging.handlers import RotatingFileHandler
 from threading import Timer
 
 from gpiozero import Button,OutputDevice
-from flask import Flask, g
+from flask import Flask, current_app
+
+app=Flask(__name__)
 
 class Door():
-    last_action = None
-    last_trigger_time = None
+    last_action = None 
 
     def __init__(self,  config):
             self.name = config['door']['name']
@@ -23,6 +25,8 @@ class Door():
             self.time_to_open = config['door']['time_to_open']
             self.time_to_close = config['door']['time_to_close']
             self.switch = Button(self.pin_state)
+            self.switch_thread =None
+            self.last_trigger_time = datetime.today()
             self.state = "close"
 
     def get_state(self):
@@ -38,60 +42,60 @@ class Door():
         switch = OutputDevice(self.pin_relay)
         time.sleep(0.5)
         switch.on()
-
+        self.last_trigger_time = datetime.today()
         state = self.get_state()
-        global switch_thread
+
         if self.state == "close":
-            switch_thread= Timer(self.time_to_open, self.set_state, [ "open",])
-            switch_thread.start()
+            self.switch_thread= Timer(self.time_to_open, self.set_state, [ "open",])
+            self.switch_thread.start()
             self.state = "openning"
         elif self.state == "openning":
-            switch_thread.cancel()
+            self.switch_thread.cancel()
             self.state = "open" 
         elif self.state == "open": 
-            switch_thread= Timer(self.time_to_close, self.set_state, ["close",])
-            switch_thread.start() 
+            self.switch_thread= Timer(self.time_to_close, self.set_state, ["close",])
+            self.switch_thread.start() 
             self.state = "closing" 
         elif self.state == "closing":
-            switch_thread.cancel()
+            self.switch_thread.cancel()
             self.state = "open"
         return self.state
 
-def init_app():
-    app = Flask(__name__)
+    def get_trigger_time(self):
+        return str(self.last_trigger_time)
+
+def init_app(app):
+    app.debug = True
     config = None
-    print("Initializing...")
     with open('config.json','r') as f:
         try:
             config = json.load(f)
         except ValueError as e:
             app.logger.error(e)
     with app.app_context():
-        g.garage_door = Door(config)
-    handler = RotatingFileHandler('/var/log/garage_door.log', maxBytes=10000, backupCount=1)
-    handler.setLevel(logging.DEBUG)
-    app.logger.addHandler(handler)
-    print("Initialzed...")
-    return app
+        current_app.config["door"]= Door(config)
 
-app = Flask(__name__)
-#app = init_app()
-
-
-
+init_app(app)
 
 @app.route("/app/state")
 def get_state():
     with app.app_context():
-        return g.garage_door.get_state()
+        door= current_app.config["door"]
+        print(door)
+        return door.get_state()
         
 
 @app.route("/app/trigger")
 def trigger():
     with app.app_context():
-        state = g.garage_door.trigger_switch()
+        state = current_app.config["door"].trigger_switch()
         print(state)
         return state
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/app/trigger_time")
+def trigger_time():
+    with app.app_context():
+        door= current_app.config["door"]
+        return door.get_trigger_time()
+
+
--- static/js/footer.js
+++ static/js/footer.js
@@ -1,26 +1,55 @@
+state =  setInterval(load_door_status, 1000);
+
+function stop_get_state(){
+    clearInterval(state);
+}
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
 
 function load_last_trigger_time(){
-    var now = new Date();
-    var options = {
-            weekday: "long", year: "numeric", month: "short",
-            day: "numeric", hour: "2-digit", minute: "2-digit"
-    };
 
-    var footer = document.getElementById("footer");
-    footer.innerHTML = "Last trigger: "+ now.toLocaleTimeString("en-us", options);
+    var client = new HttpClient();
+    client.get('http://192.168.10.64:8081/app/trigger_time', function(response){
+            console.log(response);
+            var footer = document.getElementById("footer");
+            footer.innerHTML = "Last trigger: "+ response; 
+            });
 }
 
 function active(){
     var prg_bar = document.getElementById("status");
-    var prg_class = prg_bar.getAttribute("class").split(' '); 
-    if ( prg_class.length < 3 ){
-        prg_bar.setAttribute("class", "progress-bar progress-bar-striped active")
-    } else {
-        prg_bar.setAttribute("class", "progress-bar progress-bar-striped")
-    }
+
+    var client = new HttpClient();
+    client.get('http://192.168.10.64:8081/app/trigger', function(response){
+            console.log(response);
+            if (response == "openning" || response == "closing"){
+                prg_bar.innerHTML = response;
+                prg_bar.setAttribute("class", "progress-bar progress-bar-striped active");
+                state =  setInterval(load_door_status, 1000);
+            }
+        });
 } 
 
 function load_door_status(){
-
+    var prg_bar = document.getElementById("status");
+    var client = new HttpClient();
+    return client.get('http://192.168.10.64:8081/app/state', function(response){
+                console.log(response);
+                if (response == "open" || response == "close"){
+                    prg_bar.innerHTML = response;
+                    prg_bar.setAttribute("class", "progress-bar progress-bar-striped");
+                    stop_get_state();
+                }
+        });
 }
 
